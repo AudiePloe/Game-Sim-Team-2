305cb14d970722c6df8c133df6ff1931a63f122d
Audie Ploe <aploe@student.alamo.edu>
2022-01-24 13:08:32 -0600

    Player movement

--- GameSimTeam2/Assets/Scripts/PlayerMovement.cs
+++ GameSimTeam2/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody2D RB;
+
+    float horizontal;
+    float vertical;
+    float moveLimiter;
+
+    public float speed;
+
+
+
+    
     void Start()
     {
-        
+        RB = GetComponent<Rigidbody2D>();
+        moveLimiter = speed * 0.7f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+
+        if (horizontal != 0 && vertical != 0)
+        {
+            horizontal *= moveLimiter;
+            vertical *= moveLimiter;
+        }
+
+        RB.velocity = new Vector2(horizontal * speed, vertical * speed);
     }
 }
